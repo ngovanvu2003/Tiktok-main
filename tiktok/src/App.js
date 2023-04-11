ee00c009ad6ea06f8ad12a0c1d0552da0b64d745
vunvph21089 <117350712+vunvph21089@users.noreply.github.com>
2023-04-11 12:25:46 +0700

    chỉnh lại .babelrc với jsconfig.json

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Hi from '@/components/button'
 
 function App() {
   return (
     <div className="App">
+      <Hi/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
