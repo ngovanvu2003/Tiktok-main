00528e55ee38442cb754adc41bdd64c3ebb71461
vunvph21089 <117350712+vunvph21089@users.noreply.github.com>
2023-04-11 13:09:05 +0700

    Cấu hình sử dụng CSS/SASS

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Hi from '@/components/button'
 
 function App() {
   return (
-    <div className="App">
-      <Hi/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>HI xin chào vụ</h1>
     </div>
   );
 }
--- tiktok/src/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const button = () => {
-  return (
-    <div>Hello</div>
-  )
-}
-
-export default button
